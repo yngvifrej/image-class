e84d5ade02581cf32ea7c502fb39fa5186c36cd9
yngvifrej <71763398+yngvifrej@users.noreply.github.com>
2020-09-24 17:46:26 +0200

    init_commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const image = document.getElementById('image'); // The image we want to classify
+const result = document.getElementById('result'); // The result tag in the HTML
+const probability = document.getElementById('probability'); // The probability tag in the HTML
+
+// Initialize the Image Classifier method with MobileNet
+ml5.imageClassifier('MobileNet')
+  .then(classifier => classifier.classify(image))
+  .then(results => {
+    result.innerText = results[0].label;
+    probability.innerText = results[0].confidence.toFixed(4);
+  });
